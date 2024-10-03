691da1a86a80adf23ad040aa3bf7cdf0b8b1005b
Uranite <62639703+Uranite@users.noreply.github.com>
2024-10-03 21:59:21 +0700

    svt-av1-psyex v3.0.2-A

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://gitlab.com/AOMediaCodec/SVT-AV1.git"
-SCRIPT_COMMIT="d37b1a571dc9af5d553f5fd3cbf8c536cdb21e98"
+SCRIPT_REPO="https://github.com/BlueSwordM/svt-av1-psyex.git"
+SCRIPT_COMMIT="e07ee4b98c2a5ff6f0eedbd1e6554b9475f4a2b7"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
