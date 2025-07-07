851e7a3039a4906ebe640980b4ae3282ef49457a
Uranite <62639703+Uranite@users.noreply.github.com>
2025-07-07 18:56:28 +0700

    try

--- variants/windows-install-static.sh
+++ variants/windows-install-static.sh
@@ -4,9 +4,23 @@ package_variant() {
     IN="$1"
     OUT="$2"
 
+    # Copy the executables
     mkdir -p "$OUT"/bin
     cp "$IN"/bin/* "$OUT"/bin
 
+    # Copy the static libraries
+    mkdir -p "$OUT"/lib
+    cp -a "$IN"/lib/*.a "$OUT"/lib
+
+    # Copy the pkg-config files
+    mkdir -p "$OUT"/lib/pkgconfig
+    cp -a "$IN"/lib/pkgconfig/*.pc "$OUT"/lib/pkgconfig
+
+    # Copy the header files
+    mkdir -p "$OUT"/include
+    cp -r "$IN"/include/* "$OUT"/include
+
+    # Copy the docs
     mkdir -p "$OUT/doc"
     cp -r "$IN"/share/doc/ffmpeg/* "$OUT"/doc
 }
